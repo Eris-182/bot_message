44c0d7f516c70a266884efe3d09e99c9c7d50732
Quang Sáng <typidane@gmail.com>
2023-05-07 04:18:38 +0700

    r

--- modules/commands/console.js
+++ modules/commands/console.js
@@ -5,7 +5,7 @@ module.exports.config = {
   credits: "ManhG",
   description: "Bật tắt console user",
   commandCategory: "Hệ Thống",
-  depndencies: {  },
+  depndencies: {},
   usages: "",
   cooldowns: 2
 };
@@ -17,21 +17,21 @@ module.exports.handleEvent = async ({ event, api, Users, Threads }) => {
   var nameBox = global.data.threadInfo.get(event.threadID).threadName || "Tên không tồn tại";
   var nameUser = await Users.getNameUser(event.senderID);
   var body = event.body || "Ảnh, video hoặc nhãn dán 💌";
-  var color = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m','\x1b[31m','\x1b[1m'];
+  var color = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m', '\x1b[31m', '\x1b[1m'];
   var more = color[Math.floor(Math.random() * color.length)];
 
-  var color1 = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m','\x1b[31m','\x1b[1m'];
+  var color1 = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m', '\x1b[31m', '\x1b[1m'];
   var more1 = color1[Math.floor(Math.random() * color.length)];
 
-  var color2 = ["\x1b[34m", "\x1b[33m", "\x1b[31m", '\x1b[1m','\x1b[34m','\x1b[36m'];
+  var color2 = ["\x1b[34m", "\x1b[33m", "\x1b[31m", '\x1b[1m', '\x1b[34m', '\x1b[36m'];
   var more2 = color2[Math.floor(Math.random() * color.length)];
-  
- console.log('\x1b[32m'+ '🎀 𝐓𝐞̂𝐧 𝐁𝐨𝐱 🎃'+'\x1b[37m \x1b[' + more1 + '' + nameBox + '\x1b[31m || \x1b[0m'+''+'\x1b[37m \x1b[' + more2 + '' + nameUser + '\x1b[31m ➣ \x1b[0m' + body);
+
+  console.log('\x1b[32m' + '🎀 𝐓𝐞̂𝐧 𝐁𝐨𝐱 🎃' + '\x1b[37m \x1b[' + more1 + '' + nameBox + '\x1b[31m | \x1b[0m' + '' + '\x1b[37m \x1b[' + more2 + '' + nameUser + '\x1b[31m ➣ \x1b[0m' + body);
 };
 
 module.exports.languages = {
-  "vi": {"on": "Bật","off": "Tắt","successText": "console thành công",},
-  "en": {"on": "on","off": "off","successText": "console success!",}
+  "vi": { "on": "Bật", "off": "Tắt", "successText": "console thành công", },
+  "en": { "on": "on", "off": "off", "successText": "console success!", }
 }
 
 module.exports.run = async function ({ api, event, Threads, getText }) {
